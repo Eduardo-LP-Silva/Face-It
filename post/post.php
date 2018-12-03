605c4df3ebde63bdd26776797a3ad39bad85fb4d
ff7 <francisco.tf4@gmail.com>
2018-12-03 10:10:33 +0000

    Added templates

---
+++ post/post.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Post</title>
+    <link rel="stylesheet" href="post.css">
+  </head>
+  <body>
+    <?php include('../templates/navbar.php');?>
+    <?php include('../templates/channels.php');?>
+  </body>
+</html>
---
+++ templates/navbar.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="../templates/templates.css">
+<header>
+  <a id="title" href="front_page.html">FaceIt</a>
+  <a id="subtitle" href="front_page.html"> The face of the Internet </a>
+  <nav>
+      <a id="profile_pic" href="front_page.html"> <!-- Change to profile.html -->
+          <img src="../assets/profile_pic.png" alt="Profile Picture">
+      </a>
+      <a id="profile" href="front_page.html"> 3duardo S </a>
+      <a id="signout" href="../login/login.html"> Sign out </a>
+  </nav>
+</header>
